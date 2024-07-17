4bb90f4093cef12f0a00b534e06d8b8cfab4bae0
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2024-07-17 08:32:26 +0530

    ...

--- src/Components/login_register/Login.js
+++ src/Components/login_register/Login.js
@@ -1,39 +1,29 @@
 import { Link } from "react-router-dom";
 import "./Login.css";
+import { useState } from "react";
 
 function Login() {
+
+  const[logindata, setLogindata] = useState({
+    Email:"",
+    Password:""
+  });
+  console.log(logindata);
+
   return (
     <>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div className="loginbox">
-          <p
-            style={{
-              marginTop: "10px",
-              marginLeft: "20px",
-              fontSize: "1.5em",
-              fontWeight: "bold",
-            }}
-          >
-            Sign in
-          </p>
+          <p style={{marginTop: "10px", marginLeft: "20px", fontSize: "1.5em", fontWeight: "bold", }}> Sign in </p>
           <form style={{ marginTop: "10px", marginLeft: "20px" }}>
             <div>
-              <label>Email</label>
-              <br />
-              <input className="inputboxes" type="text" name="email" id="email" />
-            </div>
-            <br />
+              <label>Email</label><br />
+              <input className="inputboxes" onChange={(e)=>setLogindata({...logindata, Email:e.target.value})} value={logindata.Email} type="text" name="email" id="email" />
+            </div><br />
             <div>
-              <label>Password</label>
-              <br />
-              <input
-                className="inputboxes"
-                type="password"
-                name="password"
-                id="password"
-              />
-            </div>
-            <br />
+              <label>Password</label><br />
+              <input className="inputboxes" onChange={(e)=>setLogindata({...logindata, Password:e.target.value})} value={logindata.Password} type="password" name="password" id="password" />
+            </div> <br />
             <button className="continuetologin">Continue</button>
           </form>
         </div>
--- src/Components/login_register/Register.js
+++ src/Components/login_register/Register.js
@@ -1,62 +1,44 @@
 import { Link } from "react-router-dom";
 import "./Register.css";
+import { useState } from "react";
 
 function Register() {
+
+  const [registerdata, setRegisterdata] = useState({
+    name:"",
+    email:"",
+    mobile:"",
+    password:"",
+    cpassword:""
+  });
+  console.log(registerdata);
+
   return (
     <>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div className="registerbox">
-          <p
-            style={{
-              marginTop: "10px",
-              marginLeft: "20px",
-              fontSize: "1.5em",
-              fontWeight: "bold",
-            }}
-          >
-            Register
-          </p>
+          <p style={{marginTop: "10px", marginLeft: "20px", fontSize: "1.5em", fontWeight: "bold"}}> Register </p>
           <form style={{ marginTop: "10px", marginLeft: "20px" }}>
             <div>
-              <label>Name</label>
-              <br />
-              <input className="boxes" type="text" name="name" id="name" />
-            </div>
-            <br />
+              <label>Name</label><br />
+              <input className="boxes" onChange={e=>setRegisterdata({...registerdata, name:e.target.value})} value={registerdata.name} type="text" name="name" id="name" />
+            </div><br />
             <div>
-              <label>Email</label>
-              <br />
-              <input className="boxes" type="text" name="email" id="email" />
-            </div>
-            <br />
+              <label>Email</label><br />
+              <input className="boxes" onChange={e=>setRegisterdata({...registerdata, email:e.target.value})} value={registerdata.email} type="text" name="email" id="email" />
+            </div><br />
             <div>
-              <label>Mobile Number</label>
-              <br />
-              <input className="boxes" type="text" name="mobile" id="mobile" />
-            </div>
-            <br />
+              <label>Mobile Number</label><br />
+              <input className="boxes" onChange={e=>setRegisterdata({...registerdata, mobile:e.target.value})} value={registerdata.mobile} type="text" name="mobile" id="mobile" />
+            </div><br />
             <div>
-              <label>Password</label>
-              <br />
-              <input
-                className="boxes"
-                type="password"
-                name="password"
-                id="password"
-              />
-            </div>
-            <br />
+              <label>Password</label><br />
+              <input className="boxes" onChange={e=>setRegisterdata({...registerdata, password:e.target.value})} value={registerdata.password} type="password" name="password" id="password" />
+            </div> <br />
             <div>
-              <label> Confirm Password</label>
-              <br />
-              <input
-                className="boxes"
-                type="password"
-                name="password"
-                id="password"
-              />
-            </div>
-            <br />
+              <label> Confirm Password</label> <br />
+              <input className="boxes" onChange={e=>setRegisterdata({...registerdata, cpassword:e.target.value})} value={registerdata.cpassword} type="password" name="cpassword" id="cpassword" />
+            </div><br />
             <button className="continue">Continue</button>
           </form>
         </div>
